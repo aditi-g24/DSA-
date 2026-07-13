802ab26ead188c229049b67690e2c104db70d38e
aditi-g24 <aditig0107@gmail.com>
2026-07-13 18:41:30 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.65%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+        int r = min(m - 1, n - 1);
+
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (m + n - 2 - r + i) / i;
+        }
+
+        return (int)ans;
+    }
+};
