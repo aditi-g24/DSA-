90facb1d301e7ff8162d589782a958131d4c3cef
aditi-g24 <aditig0107@gmail.com>
2026-07-13 18:42:58 +0530

    Update 0062-unique-paths.cpp

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,11 +3,9 @@ public:
     int uniquePaths(int m, int n) {
         long long ans = 1;
         int r = min(m - 1, n - 1);
-
         for (int i = 1; i <= r; i++) {
             ans = ans * (m + n - 2 - r + i) / i;
         }
-
         return (int)ans;
     }
 };
