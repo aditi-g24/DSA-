7bd90c86855f7a61295ce10e9934b3269fa65f56
aditi-g24 <aditig0107@gmail.com>
2026-07-27 15:34:47 +0530

    Time: 0 ms (100%), Space: 11.8 MB (72.54%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,38 +3,24 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
+        
+        vector<vector<long long>> dp(m,vector<long long>(n,0));
 
-        vector<vector<long long>> dp(m, vector<long long>(n, 0));
-
-        // If start or destination is blocked
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
-            return 0;
-
-        dp[m - 1][n - 1] = 1;
-
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                    continue;
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
                 }
-
-                if (i == m - 1 && j == n - 1)
-                    continue;
-
-                long long right = 0, down = 0;
-
-                if (j + 1 < n)
-                    right = dp[i][j + 1];
-
-                if (i + 1 < m)
-                    down = dp[i + 1][j];
-
-                dp[i][j] = right + down;
+                else if(i == m-1 && j == n-1){
+                    dp[i][j]=1;
+                }
+                else{
+                    long long right = (j+1<n) ? dp[i][j+1] : 0;
+                    long long down = (i+1 < m) ? dp[i+1][j] : 0;
+                    dp[i][j] = right + down; 
+                }
             }
         }
-
         return dp[0][0];
     }
 };
