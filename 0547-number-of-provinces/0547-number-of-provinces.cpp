b8e8d675ac81336bc73e0b4cef4f55221b38cc76
aditi-g24 <aditig0107@gmail.com>
2026-08-03 20:44:42 +0530

    Time: 0 ms (100%), Space: 20.9 MB (21.84%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,46 @@
 class Solution {
-private: 
-    void dfs(int node, vector<vector<int>> &adjLs, vector<int> &vis){
-        vis[node]=1;
-        for(auto it : adjLs[node]){
-            if(!vis[it]){
-                dfs(it, adjLs, vis);
+private:
+    void bfs(int node, vector<vector<int>> &adjLs, vector<int> &vis){
+        queue<int> q;
+        q.push(node);
+        vis[node] = 1;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(auto it : adjLs[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
             }
         }
     }
+
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int V = isConnected.size();
         vector<vector<int>> adjLs(V);
-        for(int i  = 0; i < V; i++){
+
+        for(int i = 0; i < V; i++){
             for(int j = 0; j < V; j++){
-                if(isConnected[i][j]==1 && i!=j){
+                if(isConnected[i][j] == 1 && i != j){
                     adjLs[i].push_back(j);
                 }
             }
         }
-        
-        vector<int> vis(V,0);
+
+        vector<int> vis(V, 0);
         int count = 0;
+
         for(int i = 0; i < V; i++){
-            if(vis[i]==0){
+            if(!vis[i]){
                 count++;
-                dfs(i, adjLs, vis);
+                bfs(i, adjLs, vis);
             }
         }
-        return count;
 
-    } 
+        return count;
+    }
 };
