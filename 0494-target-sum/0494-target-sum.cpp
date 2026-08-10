444e2e135308a47fd337ac6c81ac1640035bc572
Aditi Gupta <aditig0107@gmail.com>
2026-08-10 19:54:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
-    int solve(int ind, int curSum, int target, int n, vector<int>& nums){
-        if(ind == n){
-            if(curSum == target) return 1;
-            return 0;
-        }
-
-        int plus = solve(ind + 1, curSum + nums[ind], target, n, nums);
-        int minus = solve(ind + 1, curSum - nums[ind], target, n, nums);
-
-        return plus + minus;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
 
-        return solve(0, 0,target, n, nums);
+        int totalSum = 0;
+        for(int x : nums){
+            totalSum += x;
+        }
+
+        if(abs(target) > totalSum) return 0;
+        if((totalSum - target)%2 != 0) return 0;
+        int subsetSum = (totalSum - target)/2;
+
+        vector<vector<int>> dp(n, vector<int>(subsetSum + 1, 0));
+
+        if(nums[0]==0){
+            dp[0][0] = 2;
+        }
+        else dp[0][0] = 1;
+
+        if(nums[0]!=0 && nums[0] <= subsetSum){
+            dp[0][nums[0]] = 1;
+        }
+
+        for(int ind = 1; ind < n; ind++){
+            for(int t = 0; t <= subsetSum; t++){
+                int notTake = dp[ind - 1][t];
+                int take = 0;
+                if(nums[ind] <= t){
+                    take = dp[ind][t - nums[ind]];
+                }
+                dp[ind][t] = take + notTake;
+            }
+        }
+
+        return dp[n-1][subsetSum];
     }
 };
