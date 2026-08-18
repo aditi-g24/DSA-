12cd7ee39244b0d4c71c69f16fcac6a5bf121d13
Aditi Gupta <aditig0107@gmail.com>
2026-08-18 19:09:22 +0530

    Time: 18 ms (16.62%), Space: 16.7 MB (70.55%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int solve(string word1, string word2, int i, int j, vector<vector<int>>& dp){
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
+    int solve(string& word1, string& word2, vector<vector<int>>& dp, int i, int j){
+        if(i < 0) return j+1;
+        if(j < 0) return i+1;
+
         if(dp[i][j] != -1) return dp[i][j];
 
         if(word1[i] == word2[j]){
-            return dp[i][j] = solve(word1, word2, i-1, j-1, dp);
+            return dp[i][j] = solve(word1, word2, dp, i-1, j-1);
         }
 
-        return dp[i][j] = 1 + min(solve(word1, word2, i-1, j, dp),solve(word1, word2, i, j-1, dp));
+        return dp[i][j] = 1 + min(solve(word1, word2, dp, i-1, j), solve(word1, word2, dp, i, j-1));
     }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
+
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(word1, word2, n-1, m-1, dp);
+        return solve(word1, word2, dp, n-1, m-1);
     }
 };
