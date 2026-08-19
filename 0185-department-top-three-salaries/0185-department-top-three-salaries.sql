062ba889c67910cca7d88aaef5e71851b2a5d59c
Aditi Gupta <aditig0107@gmail.com>
2026-08-19 18:18:34 +0530

    Time: 1322 ms (29.2%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,10 @@
+SELECT Department, Employee, Salary
+FROM (SELECT d.name as 'Department', e.name as 'Employee', e.salary as 'Salary',
+ DENSE_RANK() OVER(
+    PARTITION BY e.departmentID
+    ORDER BY e.salary DESC
+ ) as rnk
+FROM Employee e
+JOIN Department d
+ON e.departmentID = d.id) AS x
+WHERE rnk = 1 OR rnk = 2 OR rnk = 3;
