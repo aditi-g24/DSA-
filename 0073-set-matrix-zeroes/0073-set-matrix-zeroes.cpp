25ad78d2e403293545b6640f246690f954c54998
Aditi Gupta <aditig0107@gmail.com>
2026-08-27 09:18:40 +0530

    Time: 0 ms (100%), Space: 21.1 MB (26.29%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,7 +4,6 @@ public:
         int col = 1;
         int n = matrix.size();
         int m = matrix[0].size();
-
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
@@ -16,7 +15,6 @@ public:
                 }
             }
         }
-
         for(int i = 1; i < n; i++){
             for(int j = 1; j < m; j++){
                 if(matrix[i][j] != 0){
@@ -26,7 +24,6 @@ public:
                 }
             }
         }
-
         if(matrix[0][0] == 0){
             for(int j = 0; j < m; j++){
                 matrix[0][j] = 0;
